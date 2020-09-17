e002ed7f0edde4a9362a4b0d8b0c8fa78a6dad1b
Arzhevitin, Bogdan <Bogdan.Arzhevitin@wartsila.com>
2020-09-17 21:56:34 +0300

    Add solution for hw1

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -1,2 +1,17 @@
-const g = document.querySelector('g');
-const circle = document.querySelector('circle');
+let smileCheckBox = document.getElementById("smile-switcher");
+let smileLabel = document.querySelector("label[for='smile-switcher']");
+let smilePath = document.querySelector('path');
+
+smilePath.style.transformOrigin = "center";
+
+smileCheckBox.onclick = () => {
+  if(smileCheckBox.checked) {
+    smilePath.style.transform = "";
+    smileLabel.innerHTML = "Happy";
+  }
+  else {
+    smilePath.style.transform = "scale(1,-1) translate(0px, -130px)";
+    smileLabel.innerHTML = "Sad";
+  }
+
+}
